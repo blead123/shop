74a281950845355198e65811dbb57fbbeac3ca7f
blead123 <blead123@naver.com>
2021-12-22 22:24:49 +0900

    ItemRepository 생성 및 테스트

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -17,7 +17,7 @@ public class Item {
     @Id
     @Column(name = "item_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     @Column(nullable = false , length = 50)
     private String itemName;
@@ -30,7 +30,7 @@ public class Item {
 
     @Lob
     @Column(nullable = false)
-    private int description;
+    private String description;
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus isSold;
