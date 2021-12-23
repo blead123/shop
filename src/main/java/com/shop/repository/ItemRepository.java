22df772c9f20357732ce9d4b09739754439febbe
blead123 <blead123@naver.com>
2021-12-23 20:32:25 +0900

    상품명 조회 및 상품명 또는 상세기능을 조회하는 테스팅

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item , Long> {
+    List<Item> findByItemName(String itemName);
 
-    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNameOrDescription(String itemName,String itemDescription);
 }
